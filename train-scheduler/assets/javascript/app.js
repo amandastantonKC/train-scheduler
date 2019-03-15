fed6ca292cea4a025657c6692223395bd659be10
Amanda Stanton <44312668+amandastantonKC@users.noreply.github.com>
2019-03-15 07:23:26 -0500

    added JavaScript file

---
+++ train-scheduler/assets/javascript/app.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+
+
+    //Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyCb_2Usxp4daE5hfH1j_jEvdg49smbUojg",
+        authDomain: "train-scheduler-a3e2f.firebaseapp.com",
+        databaseURL: "https://train-scheduler-a3e2f.firebaseio.com",
+        projectId: "train-scheduler-a3e2f",
+        storageBucket: "train-scheduler-a3e2f.appspot.com",
+        messagingSenderId: "430702320916"
+    };
+    firebase.initializeApp(config);
+});
