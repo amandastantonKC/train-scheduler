a82ce391a60a510ea5aeb009cc8e03e6ed9538c6
Amanda Stanton <44312668+amandastantonKC@users.noreply.github.com>
2019-03-15 07:39:43 -0500

    update

--- train-scheduler/assets/javascript/app.js
+++ train-scheduler/assets/javascript/app.js
@@ -12,3 +12,44 @@ $(document).ready(function () {
     };
     firebase.initializeApp(config);
 });
+
+var database = firebase.database();
+
+$("#trainInfoBtn").on("click", function (event) {
+    event.preventDefault();
+});
+
+var name = $("#name").val().trim();
+
+var destination = $("#destination").val().trim();
+
+var time = moment($("#time").val().trim(), "hh:mm").subtract(1, "years").format("X");
+
+var frequency = $("#frequency").val().trim();
+
+
+var currentTime = moment();
+console.log("CURRENT TIME:" + moment(currentTime).format("hh:mm"));
+
+database.ref().push({
+    train: name,
+    trainGoing: destination,
+    trainComing: time,
+    everyXMin: frequency,
+});
+
+$("#name").val("");
+$("#destination").val("");
+$("#time").val("");
+$("#frequency").val("");
+
+return false;
+
+database.ref().on("child_added", function (childSnapshot, ) {
+    var newName = childSnapshot.val().train;
+    var destination = childSnapshot.val().trainGoing;
+    var time = childSnapshot.val().trainComing;
+    var frequency = childSnapshot.val().everyXMin;
+    
+    console.log (newName,destination,time,frequency);
+});
